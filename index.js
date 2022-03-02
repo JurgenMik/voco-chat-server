a771377124cbc3ceccc191b63da0c5d75d3090e6
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-03-02 23:41:55 +0200

    connecting frontend-backend-db

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-
 
 const MessageModel = require("./models/message");
 
 app.use(express.json()); // recieve information from front-end in json format
+app.use(cors());  //  {package} connection to front end using created api
 
 mongoose.connect("mongodb+srv://jurgen:Lollakas123@vocochat.llr97.mongodb.net/chat?retryWrites=true&w=majority",
     { // connection url - represents what mongodb are we trying to connect to
@@ -13,12 +14,17 @@ mongoose.connect("mongodb+srv://jurgen:Lollakas123@vocochat.llr97.mongodb.net/ch
      }
   );
 
-app.get("/", async (req, res) => {
-   const message = new MessageModel({messageSisu: "Tervist"});
+app.post("/insert", async (req, res) => {
+
+    const sonum = req.body.sonum; // catch request
+    const user = req.body.user; // catch request
+
+    const message = new MessageModel({messageSisu: sonum, kasutaja: user});
 
     try{
 
        await message.save();
+       res.send("inserted data")
 
     } catch(err){
        console.log(err);
--- models/message.js
+++ models/message.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose')
 
-
 const messageSchema = new mongoose.Schema({
     messageSisu: {
+
         type: String,
         required: true,
         maxlength: 100,
+    },
+    kasutaja: {
+
+        type: String,
+        required: true,
     },
 });
 
